e12b20481e08ac11f3c203ed9a3e69e8b41a3b0e
AlexPortero24 <ad.porterod@uea.edu.ec>
2025-06-25 14:44:47 -0500

    Aqui se puso la funciones para convertir de Celsius a Fahrenheit y Kelvin y se utiliza snake_case para poner el nombre de la funcion

--- Convertir_Unidades.py
+++ Convertir_Unidades.py
@@ -4,3 +4,13 @@
 
 # La idea surgió con el propósito de facilitar la conversión de unidades en la materia de Física 2.
 # otra asignatura que también estamos cursando.
+
+# Función que convierte de Celsius a Fahrenheit
+# Aquí se utiliza snake_case para el nombre de la función: celsius_a_fahrenheit
+def celsius_a_fahrenheit(grados_celsius):  # grados_celsius es tipo float
+    return (grados_celsius * 9/5)+32   # El resultado también es un float
+
+# Función que convierte de Celsius a Kelvin
+# También usa snake_case para el nombre de la función: celsius_a_kelvin
+def celsius_a_kelvin(grados_celsius):  # grados_celsius es float
+    return grados_celsius+273.15     # Retorna un float
